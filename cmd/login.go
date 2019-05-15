b52ccde409a46c37aee7b5038e958719fd5f6086
Kim Pepper <kim@pepper.id.au>
2019-05-15 17:43:28 +1000

    WIP

--- cmd/login.go
+++ cmd/login.go
@@ -5,6 +5,7 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/cognitoidentity"
 	"github.com/aws/aws-sdk-go/service/cognitoidentityprovider"
 	"gopkg.in/alecthomas/kingpin.v2"
 	"os"
@@ -12,8 +13,8 @@ import (
 
 type cmdLogin struct {
 	Username string
-	Password		string
-	ClientID         string
+	Password string
+	ClientID string
 }
 
 func (v *cmdLogin) run(c *kingpin.ParseContext) error {
@@ -21,26 +22,26 @@ func (v *cmdLogin) run(c *kingpin.ParseContext) error {
 	if err != nil {
 		fmt.Println(err)
 	}
-	svc := cognitoidentityprovider.New(sess, aws.NewConfig().WithRegion("ap-southeast-2"))
+	config := aws.NewConfig().WithRegion("ap-southeast-2")
+	cognitoIdentityProvider := cognitoidentityprovider.New(sess, config)
 
 	authInput := new(cognitoidentityprovider.InitiateAuthInput)
 	authInput.SetAuthFlow(cognitoidentityprovider.AuthFlowTypeUserPasswordAuth)
 	authInput.SetClientId(v.ClientID)
 
-	authParams := map[string]*string {
-		"USERNAME" : &v.Username,
+	authParams := map[string]*string{
+		"USERNAME": &v.Username,
 		"PASSWORD": &v.Password,
 	}
 
 	authInput.SetAuthParameters(authParams)
-	authOutput, err := svc.InitiateAuth(authInput)
+	authOutput, err := cognitoIdentityProvider.InitiateAuth(authInput)
 
 	if err != nil {
 		if awsErr, ok := err.(awserr.Error); ok {
 			switch awsErr.Code() {
 			case cognitoidentityprovider.ErrCodePasswordResetRequiredException:
 				fmt.Println("You are required to change your password.")
-				fmt.Println("https://pnx.auth.ap-southeast-2.amazoncognito.com/login?response_type=code&client_id=1u6m5v0naeftt0409udaeck52t&redirect_uri=http://localhost")
 			}
 		}
 		fmt.Println(err)
@@ -52,9 +53,30 @@ func (v *cmdLogin) run(c *kingpin.ParseContext) error {
 	result := authOutput.AuthenticationResult
 	accessToken := result.AccessToken
 
+
+	//getCredsInput := cognitoidentity.GetCredentialsForIdentityInput{
+	//	IdentityId:
+	//}
+	//
+	//cognitoidentity.GetCredentialsForIdentity(getCredsInput)
+	//{
+	//identity_id: "IdentityId",
+	//logins: {
+	//	"IdentityProviderName" => "IdentityProviderToken",
+	//},
+	//custom_role_arn: "ARNString",
+	//})
+
+	//idToken := result.IdToken
+	//sess.Config.WithCredentials(cognitoidentity.Credentials{
+	//
+	//})
+
+	creds := new(cognitoidentity.Credentials)
+	creds.SetSessionToken()
 	userInput := new(cognitoidentityprovider.GetUserInput)
 	userInput.SetAccessToken(*accessToken)
-	userOutput, err := svc.GetUser(userInput)
+	userOutput, err := cognitoIdentityProvider.GetUser(userInput)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
@@ -69,8 +91,7 @@ func Login(app *kingpin.Application) {
 	v := new(cmdLogin)
 
 	command := app.Command("login", "Logs in a user.").Action(v.run)
-	//clientSecret = command.Flag("client-secret", "ClientId for authentication").Required().Envar("SKPR_CLIENT_SECRET").String()
-	command.Flag("client-id", "ClientId for authentication").Required().Envar("SKPR_CLIENT_ID").StringVar(&v.ClientID)
-	command.Flag("username", "Username for authentication").Required().Envar("SKPR_USERNAME").StringVar(&v.Username)
-	command.Flag("password", "Password for authentication").Required().Envar("SKPR_PASSWORD").StringVar(&v.Password)
+	command.Flag("clientid", "Client ID for authentication").Required().StringVar(&v.ClientID)
+	command.Flag("username", "Username for authentication").Required().StringVar(&v.Username)
+	command.Flag("password", "Password for authentication").Required().StringVar(&v.Password)
 }
